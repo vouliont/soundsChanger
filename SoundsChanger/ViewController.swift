a4940ac67ddeac559212e48df6a155649ebcd0be
Vlad <vladporaiko@gmail.com>
2018-12-11 06:44:17 -0800

    create buttons, navigator view controller, add second view controller

--- SoundsChanger/ViewController.swift
+++ SoundsChanger/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
+    @IBAction func recordAudio(_ sender: UIButton) {
+        recordingLabel.text = "Recording in Progress"
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
+    }
 
+    @IBAction func stopRecording(_ sender: UIButton) {
+        recordingLabel.text = "Tap to Record"
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+    }
 }
 
